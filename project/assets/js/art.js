b053357f97c3b768f671616a22db745b5576b1ef
seamuskills <thedarkknite10@gmail.com>
2025-02-11 02:16:21 -0600

    art page finished

---
+++ project/assets/js/art.js
@@ -0,0 +1,9 @@
+
+function zoom(src){
+    document.getElementById("zoombg").style.visibility = "visible"
+    document.getElementById("zoomimg").src = src
+}
+
+function hide(){
+    document.getElementById("zoombg").style.visibility = "hidden"
+}
